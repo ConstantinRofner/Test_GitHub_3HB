0845dce6c9216375041e1f040b54d924e8b9e804
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-01-21 23:12:38 +0100

    wrote Methods and declared Variables for class Rectangle

--- src/GeoMetry/Rectagle.java
+++ src/GeoMetry/Rectagle.java
@@ -2,4 +2,26 @@ package GeoMetry;
 
 public class Rectagle {
 
+	double length;
+	double height;
+	
+	
+	public double getLength() {
+		return length;
+	}
+	public void setLength(double length) {
+		this.length = length;
+	}
+	public double getHeight() {
+		return height;
+	}
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	
+	
+	double getCircumference(double lentgh, double heigth) {
+		double circumference=length*2+heigth*2;
+		return circumference;
+	}
 }
