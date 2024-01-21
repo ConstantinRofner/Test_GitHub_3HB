455bbaa1ba66fdf6ea4bd02a2b0bb3c1b71124ab
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-01-21 23:41:55 +0100

    added and changed the methods for class Circle

--- src/GeoMetry/Circle.java
+++ src/GeoMetry/Circle.java
@@ -1,5 +1,34 @@
 package GeoMetry;
 
-public class Circle {
+public class Circle extends forAllObjects{
 
+	double radius;
+	
+	
+	public double getRadius() {
+		return radius;
+	}
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+	
+	/**
+	 * calculates the circumference of the circle
+	 * @param radius
+	 * @return
+	 */
+	public double getCircumference(double radius) {
+		double circumference=radius+2*3.14;
+		return circumference;
+	}
+	
+	/**
+	 * calculates the area of the circle
+	 * @param radius
+	 * @return
+	 */
+	public double getArea(double radius) {
+		double area=3.14*radius*radius;
+		return area;
+	}
 }
