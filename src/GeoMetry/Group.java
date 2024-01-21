daced80d64424bc11f1e70ddb70e8afd13b08873
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-01-22 00:24:03 +0100

    added methods and declared variables for class Group(unfinished)

--- src/GeoMetry/Group.java
+++ src/GeoMetry/Group.java
@@ -1,5 +1,40 @@
 package GeoMetry;
 
-public class Group {
+import java.util.ArrayList;
 
+public class Group {
+	
+    private ArrayList<Object> objects = new ArrayList<>();
+    
+    
+	public ArrayList<Object> getObjects() {
+		return objects;
+	}
+	public void setObjects(ArrayList<Object> objects) {
+		this.objects = objects;
+	}
+    
+	public void addObject(ArrayList<Object> objects) {
+		objects.add(null);
+	}
+	
+	public void deleteObject(ArrayList<Object> objects) {
+		objects.remove(null);
+	}
+	
+	public void getAreaOfAllObjects(ArrayList<Object> objects) {
+		
+	}
+	
+	public void getCirumferenceOfAllObjects() {
+		
+	}
+	
+	public void biggestObject() {
+		
+	}
+	
+	public void printOutAllObjects() {
+		
+	}
 }
