600c9132f42c27dec4fc26fc5be5b4e6df1bbf17
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-01-21 23:08:35 +0100

    wrote Methods and declared Variables for class forAllObjects

--- src/GeoMetry/forAllObjects.java
+++ src/GeoMetry/forAllObjects.java
@@ -2,4 +2,26 @@ package GeoMetry;
 
 public class forAllObjects {
 
+	double xValue;
+	double yValue;
+	
+	
+	public double getxValue() {
+		return xValue;
+	}
+	public void setxValue(double xValue) {
+		this.xValue = xValue;
+	}
+	public double getyValue() {
+		return yValue;
+	}
+	public void setyValue(double yValue) {
+		this.yValue = yValue;
+	}
+	
+	
+	double getArea(double xValue, double yValue) {
+		double area=xValue*yValue;
+		return area;
+	}
 }
