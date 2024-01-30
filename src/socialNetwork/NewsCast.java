ff7825cc34725c6b07564ee35161351d69e5bec8
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-01-30 09:31:11 +0100

    added a constructor

--- src/socialNetwork/NewsCast.java
+++ src/socialNetwork/NewsCast.java
@@ -3,5 +3,8 @@ package socialNetwork;
 public class NewsCast extends News{
 
 	private String textMessage;
-	
+
+	public NewsCast(String author, String textMessage) {
+		this.setAuthor(author);
+	}
 }
