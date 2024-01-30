7f8f0a2ddd43eb8efc5760871d0923674b74ef45
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-01-30 09:21:54 +0100

    added a method which can search for authors who are in the ArrayList<News>

--- src/socialNetwork/NewsFeed.java
+++ src/socialNetwork/NewsFeed.java
@@ -34,4 +34,18 @@ public class NewsFeed {
 	public void remove(News n) {
 		this.news.remove(n);
 	}
+	
+	/**
+	 * Method searches for all authors who are in the ArrayList
+	 * @param author
+	 */
+	public void search(String author) {
+		ArrayList <News> searchByAuthor = new ArrayList<News>();
+		for (News n : this.news) {
+			if (n.getAuthor().equals(author)) {
+				searchByAuthor.add(n);
+			}
+		}
+		System.out.println(searchByAuthor);
+	}
 }
