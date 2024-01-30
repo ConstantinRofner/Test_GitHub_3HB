46b33453ae787c44dda67999a3383062ffcdc40d
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-01-30 08:48:47 +0100

    added the getter and setters

--- src/socialNetwork/News.java
+++ src/socialNetwork/News.java
@@ -9,5 +9,31 @@ public abstract class News {
 	private LocalDate timestamp;
 	private int likes;
 	private ArrayList <String> comments;
-
+	
+	
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public LocalDate getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(LocalDate timestamp) {
+		this.timestamp = timestamp;
+	}
+	public int getLikes() {
+		return likes;
+	}
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+	public ArrayList<String> getComments() {
+		return comments;
+	}
+	public void setComments(ArrayList<String> comments) {
+		this.comments = comments;
+	}
+	
 }
