ae20430f607c4a435822d54035459976c59e9f91
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-01-30 09:42:49 +0100

    generated a constructor and imported java.time.LocalDate

--- src/socialNetwork/PhotoCast.java
+++ src/socialNetwork/PhotoCast.java
@@ -1,7 +1,20 @@
 package socialNetwork;
 
+import java.time.LocalDate;
+
 public class PhotoCast extends News{
 
 	private String photoFileName;
 	private String photoHeadline;
+	
+	public PhotoCast(String author, String photoFileName, String photoHeadline) {
+		this.setAuthor(author);
+		this.setTimestamp(LocalDate.now());
+		this.setLikes(0);
+		this.setComments(null);
+		this.photoFileName = photoFileName;
+		this.photoHeadline = photoHeadline;
+	}
+	
+	
 }
