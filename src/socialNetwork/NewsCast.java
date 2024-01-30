77765d676f086b995d577078e59df416e53b213b
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-01-30 09:35:10 +0100

    fixed the constructor and imported java.time.LocalDate

--- src/socialNetwork/NewsCast.java
+++ src/socialNetwork/NewsCast.java
@@ -1,18 +1,25 @@
 package socialNetwork;
 
+import java.time.LocalDate;
+
 public class NewsCast extends News{
 
 	private String textMessage;
 
 	public NewsCast(String author, String textMessage) {
 		this.setAuthor(author);
+		this.setTimestamp(LocalDate.now());
+		this.setLikes(0);
+		this.setComments(null);
+		this.textMessage = textMessage;
 	}
-
+	
 	public String getTextMessage() {
 		return textMessage;
 	}
 	public void setTextMessage(String textMessage) {
 		this.textMessage = textMessage;
 	}
+	
 	
 }
