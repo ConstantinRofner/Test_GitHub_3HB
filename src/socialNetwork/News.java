d62578b8704562a21867256c92e2c396db7d2d9e
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-01-30 08:47:17 +0100

    added all variables and imported java.LocalDate and java.ArrayList

--- src/socialNetwork/News.java
+++ src/socialNetwork/News.java
@@ -1,5 +1,13 @@
 package socialNetwork;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public abstract class News {
+	
+	private String author;
+	private LocalDate timestamp;
+	private int likes;
+	private ArrayList <String> comments;
 
 }
