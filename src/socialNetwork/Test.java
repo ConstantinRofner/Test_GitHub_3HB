15a7c609093363778b809ca9e35dde27d629fcf1
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-02-06 09:03:42 +0100

    added some variables for testing

--- src/socialNetwork/Test.java
+++ src/socialNetwork/Test.java
@@ -3,7 +3,10 @@ package socialNetwork;
 public class Test {
 
 	public static void main(String[] args) {
-
+		NewsFeed nf = new NewsFeed();
+		
+		News n = new NewsCast("Constantin", "TESTTEXT TESTTEXT");
+		News p = new PhotoCast("Constantin", "image1.png", "Class Photo");
 	}
 
 }
