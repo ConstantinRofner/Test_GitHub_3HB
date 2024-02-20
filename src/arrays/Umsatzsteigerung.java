7bf8e8f1c4c4facc1f5e81beb197cfa660402445
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-02-20 09:34:51 +0100

    added a Bedingung to the method

--- src/arrays/Umsatzsteigerung.java
+++ src/arrays/Umsatzsteigerung.java
@@ -7,13 +7,16 @@ public class Umsatzsteigerung {
 		
 		public int count5PercentJumps(int[]gains) {
 			int count=0;
-			for(int i=0; i<30; i++) {
-				for(int j=1; j<29; j++) {
-					if((dailyGains[i] * 1.05) >= dailyGains[j]) {
+			if(this.dailyGains.length < 2) {
+				System.out.println("Es sind zu wenig Zahlen für eine Berechnung vorhanden.");
+				for(int i=0; i<30; i++) {
+					for(int j=1; j<29; j++) {
+						if((dailyGains[i] * 1.05) >= dailyGains[j]) {
 						count++;
 					}
 				}
 			}
-			return count;
 		}
+		return count;
 	}
+}
