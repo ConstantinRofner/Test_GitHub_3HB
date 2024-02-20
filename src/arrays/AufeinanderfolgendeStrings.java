faeb33f1b13765cf5f729da8068265bdcbb8d896
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-02-20 09:43:59 +0100

    started working on the class

--- src/arrays/AufeinanderfolgendeStrings.java
+++ src/arrays/AufeinanderfolgendeStrings.java
@@ -1,5 +1,10 @@
 package arrays;
 
 public class AufeinanderfolgendeStrings {
-
+	
+	String[]signs1= {"F", "DO", "MOS", "MOS", "MOS", "MOS", "WES"};
+	
+	public boolean isProbablyApproaching(String signs) {
+		return true;
+	}
 }
