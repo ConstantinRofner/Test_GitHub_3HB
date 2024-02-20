b89a5fb978ceba1bfeb5aa3040f52fcb14585804
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-02-20 09:28:56 +0100

    added a method to count 5 percent jumps

--- src/arrays/Umsatzsteigerung.java
+++ src/arrays/Umsatzsteigerung.java
@@ -2,7 +2,18 @@ package arrays;
 
 public class Umsatzsteigerung {
 
-	public static void main(String[] args) {
 		
+		int[] dailyGains = { 1000, 2000, 500, 9000, 9010};
+		
+		public int count5PercentJumps(int[]gains) {
+			int count=0;
+			for(int i=0; i<30; i++) {
+				for(int j=1; j<29; j++) {
+					if((dailyGains[i] * 1.05) >= dailyGains[j]) {
+						count++;
+					}
+				}
+			}
+			return count;
+		}
 	}
-}
