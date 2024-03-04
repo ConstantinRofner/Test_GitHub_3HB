dad5280ec2d255e318588a3dd455f09e2c36d70f
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-03-04 11:44:29 +0100

    added an ArrayList

--- src/ArrayList/ManipulateLosses.java
+++ src/ArrayList/ManipulateLosses.java
@@ -1,5 +1,24 @@
 package ArrayList;
 
-public class ManipulateLosses {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ManipulateLosses {
+	
+	private List<Double>list;
+	public List trimNonGrowingNumbers() {
+		this.list = new ArrayList<Double>();
+	}
+	
+	public List<Double>getList(){
+		return this.list;
+	}
+	
+	public void setList(List<Double>list) {
+		this.list = list;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 }
