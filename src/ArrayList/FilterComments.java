aca2af6f6847747b3d1127f35b7563d6afd114b3
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-03-04 11:33:53 +0100

    coded the class

---
+++ src/ArrayList/FilterComments.java
@@ -0,0 +1,21 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilterComments {
+
+	public static void reduceToComments (List<String> lines) {
+		if (lines.size() > 3) {
+			for (int i = 0; i < 3; i++) {
+				lines.remove(i);
+			}
+		} else {
+			System.out.println("Illegal size " + lines.size() + " of list, must be divisble by 4");
+		}
+	}
+	
+	public static void main(String[] args) {
+		List<String> lines = new ArrayList<String>();
+	}
+}
