367fd52bef54fdd6cf32c3b8fe28da005fc53603
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-03-05 08:38:24 +0100

    created class and added LinkedList

---
+++ src/linkedList/LinkedListTester.java
@@ -0,0 +1,16 @@
+package linkedList;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListTester {
+
+	public static void main(String[] args) {
+		
+		List <Integer> list = new LinkedList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		
+	}
+}
