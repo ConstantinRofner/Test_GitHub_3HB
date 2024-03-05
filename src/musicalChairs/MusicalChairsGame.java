284c50cb275cc2ef65c1904a69d97574d44d1465
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-03-05 09:20:11 +0100

    generated getter and setter/created List

--- src/musicalChairs/MusicalChairsGame.java
+++ src/musicalChairs/MusicalChairsGame.java
@@ -1,5 +1,19 @@
 package musicalChairs;
 
+import java.util.List;
+
 public class MusicalChairsGame {
 
+	private List<String> names;
+
+	
+	public List<String> getNames() {
+		return names;
+	}
+
+	public void setNames(List<String> names) {
+		this.names = names;
+	}
+	
+	
 }
