ace28b3142f3ab3705a9c3f7abc9e94e2ffea7d3
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-03-11 11:40:17 +0100

    added a pattern and created the next method, imported two functions

---
+++ src/linkedList/CheesInserter.java
@@ -0,0 +1,18 @@
+package linkedList;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class CheesInserter {
+
+	private static final Pattern vegetables = Pattern.compile(
+			"Zuchini|Paprikas?|Zwiebeln?|Tomaten?"
+			);
+	
+	public static void insertCheeseAroundVegetable(List<String> ingredients) {
+		
+	}
+	public static void main(String[] args) {
+		
+	}
+}
