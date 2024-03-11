a64d57383c25396b0b559cdf06f9433694fe1eea
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-03-11 11:43:11 +0100

    completed for and added comments

--- src/linkedList/CheesInserter.java
+++ src/linkedList/CheesInserter.java
@@ -1,6 +1,7 @@
 package linkedList;
 
 import java.util.List;
+import java.util.ListIterator;
 import java.util.regex.Pattern;
 
 public class CheesInserter {
@@ -10,7 +11,12 @@ public class CheesInserter {
 			);
 	
 	public static void insertCheeseAroundVegetable(List<String> ingredients) {
-		
+		//Initialisierung Laufvariable
+		//Abbruchbedingung (hasNext())
+		//keine Inkrementierung -> passiert im Schleifenkörper
+		for(ListIterator<String> iterator = ingredients.listIterator();iterator.hasNext();) {
+			
+		}
 	}
 	public static void main(String[] args) {
 		
