889d44e48456a877589b4d55bbf80d5547c9d118
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-03-12 09:28:18 +0100

    added a constructor for ArrayQueue

--- src/queue/ArrayQueue.java
+++ src/queue/ArrayQueue.java
@@ -6,4 +6,11 @@ public class ArrayQueue {
 	private static int rear;
 	private static int capacity;
 	private static int[] queue;
+	
+	public ArrayQueue(int size) {
+		front = 0;
+		rear = 0;
+		capacity = size;
+		queue = new int[size];
+	}
 }
