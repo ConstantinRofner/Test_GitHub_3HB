1ac3797fbc3916db8d0d54d8a26c81fc1e598ab0
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-04-08 08:40:37 +0200

    created an arrayqueue

--- src/queue/ArrayQueueTest.java
+++ src/queue/ArrayQueueTest.java
@@ -4,5 +4,13 @@ public class ArrayQueueTest{
 	
 	public static void main(String[] args) {
 	
+		ArrayQueue ArrayQueue = new ArrayQueue(5);
+
+		ArrayQueue.queueEnqueue(2);
+		ArrayQueue.queueEnqueue(5);
+		
+		ArrayQueue.queueFront();
+		ArrayQueue.queueDequeue();
+		ArrayQueue.queueDisplay();
 	}
 }
