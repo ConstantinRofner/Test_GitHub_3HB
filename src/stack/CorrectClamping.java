f33a752d82a6c0b5300877e1c307788486f1e0b0
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-04-15 10:28:59 +0200

    added a stack

--- src/stack/CorrectClamping.java
+++ src/stack/CorrectClamping.java
@@ -1,9 +1,25 @@
 package stack;
 
+import java.util.Stack;
+
 public class CorrectClamping {
 
+	public static boolean ClampingCorrect(String input) {
+		int roundOpenBrackets = 0;
+		int roundClosedBrackets = 0;
+		int curvedOpenBrackets = 0;
+		int curvedClosedBrackets =0;
+		int squareOpenBrackets = 0;
+		int squareClosedBrackets = 0;
+		
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+	}
+	
     public static void main(String[] args) {
-    	
     	
     }
 }
