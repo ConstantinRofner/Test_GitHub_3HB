8fdfed5d48001f42b385e3a7144ad86506d875c0
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-04-16 08:46:48 +0200

    completed calculateFaculty

--- src/rekursion/Faculty.java
+++ src/rekursion/Faculty.java
@@ -2,13 +2,14 @@ package rekursion;
 
 public class Faculty {
 
-	public static int Faculty(int n) {
-		if(n <= 1) {
-			return n;
+	public static int calculateFaculty(int n) {
+		if(n == 0) {
+			return 1;
 		}else {
-			
+			return n * calculateFaculty(n-1);
 		}
 	}
+	
 	public static void main(String[] args) {
 
 	}
