a19094cab8326e6c0d5e1fd91dc711c1c635c0fe
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-04-16 09:21:50 +0200

    created a new class

---
+++ src/rekursion/Sum.java
@@ -0,0 +1,16 @@
+package rekursion;
+
+public class Sum {
+	
+	public static int sum(int n) {
+		if(n == 0) {
+			return 0;
+		}else {
+			return n + sum(n-1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(sum(10));
+	}
+}
