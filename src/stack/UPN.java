05df2df2524a9968872e4c667b007856cd557f97
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-04-23 08:41:31 +0200

    added the start

--- src/stack/UPN.java
+++ src/stack/UPN.java
@@ -1,10 +1,23 @@
 package stack;
 
+import java.util.LinkedList;
+import java.util.regex.Pattern;
+
 public class UPN {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static int calculateUPN(String input) {
+		LinkedList<Integer> stack = new LinkedList<>();
+		//operatorPattern: überprüft auf mathematische Rechenzeichen
+		Pattern operatorPattern = Pattern.compile("[+*-/]");
+		//numericPattern: //d...Ziffer[0-9], + beliebig viele Ziffern
+		Pattern numericPattern = Pattern.compile("\\d+");
+		
+		for(String token : input.split("\\s+")) {
+			
+		}
 	}
-
+	
+	public static void main(String[] args) {
+		
+	}
 }
