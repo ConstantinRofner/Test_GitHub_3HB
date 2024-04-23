b1ec2916e201a2705ed87ef0ca51d61dd03114a8
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-04-23 08:27:54 +0200

    new classes

--- src/stack/CorrectClamping.java
+++ src/stack/CorrectClamping.java
@@ -1,45 +1,32 @@
 package stack;
 
+import java.util.LinkedList;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class CorrectClamping {
 
-    public static boolean ClampingCorrect(String input) {
-        Stack<Character> stack = new Stack<>();
-
-        for (char c : input.toCharArray()) {
-            switch (c) {
-                case '(':
-                case '{':
-                case '[':
-                    stack.push(c);
-                    break;
-                case ')':
-                    if (stack.isEmpty() || stack.pop() != '(') return false;
-                    break;
-                case '}':
-                    if (stack.isEmpty() || stack.pop() != '{') return false;
-                    break;
-                case ']':
-                    if (stack.isEmpty() || stack.pop() != '[') return false;
-                    break;
-            }
-        }
-
-        return stack.isEmpty();
-    }
-
+	public static boolean checkBrackets(String input) {
+		LinkedList<Character> stack = new LinkedList<>();
+		for(char c : input.toCharArray()) {
+			if(c == '(' || c == '{' || c == '[') {
+				stack.push(c);
+			}else if(c == ')' || c == '}' || c == ']') {
+				if(stack.isEmpty()) {
+					return false;
+				}
+				char lastOpen = stack.peek();
+				if((c == ')' && lastOpen != '('||
+						c == '}' && lastOpen != '}'||
+						c == ']' && lastOpen != ']')){
+					return false;
+				}
+				stack.pop();
+			}
+		}
+		return stack.isEmpty();
+	}
+	
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Geben Sie einen Code ein, welcher überprüft werden soll:");
-        String input = scanner.nextLine();
-        scanner.close();
-
-        if (ClampingCorrect(input)) {
-            System.out.println("Korrekte Klammerung");
-        } else {
-            System.out.println("Fehlerhafte Klammerung");
-        }
+    	
     }
 }
