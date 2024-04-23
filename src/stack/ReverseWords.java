67a9c34dba56c2b9f5c8a6be47f9928789b839bc
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-04-23 08:09:04 +0200

    new class

--- src/stack/ReverseWords.java
+++ src/stack/ReverseWords.java
@@ -1,42 +1,23 @@
 package stack;
 
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.LinkedList;
 
 public class ReverseWords {
+
+	public static String reverseString(String input) {
+		LinkedList<Character> stack = new LinkedList<Character>();
+		for(char c : input.toCharArray()) {
+			stack.push(c);
+		}
+		StringBuilder reversedString = new StringBuilder();
+		while(!stack.isEmpty()) {
+			reversedString.append(stack.pop());
+		}
+		return reversedString.toString();
+	}
 	
-	/**
-	 * methods contains other methods which reverse the string
-	 * @param input
-	 * @return
-	 */
-    public static String reverseString(String input) {
-        /**
-         * Method creates a stack
-         */
-        Stack<Character> stack = new Stack<>();
-        /**
-         * for-each method transforms all elements into chars
-         */
-        for (char c : input.toCharArray()) {
-            stack.push(c);
-        }
-        /**
-         * creates an String which saves the letters in the reversed order
-         */
-        StringBuilder reverseString = new StringBuilder();
-        while (!stack.isEmpty()) {
-            reverseString.append(stack.pop());
-        }
-        return reverseString.toString();
-    }
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Geben Sie einen String ein, der umgekehrt werden soll:");
-        String input = scanner.nextLine();
-
-        String reversed = reverseString(input);
-        System.out.println("Der umgekehrte String lautet: " + reversed);
+    	System.out.println("Hello World");
+    	System.out.println(reverseString("Hello World"));
     }
 }
