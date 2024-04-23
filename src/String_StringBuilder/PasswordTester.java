f85917a8d36c798e0a52455c464edb419ca6c722
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-04-23 09:40:23 +0200

    changed the type of the method

--- src/String_StringBuilder/PasswordTester.java
+++ src/String_StringBuilder/PasswordTester.java
@@ -1,12 +1,13 @@
 package String_StringBuilder;
 
 public class PasswordTester {
-	public static String isGoodPassword(String password) {
+	public static boolean isGoodPassword(String password) {
 		  String length = ".{8,}";
 	      String uppercase = "[A-Z]";
 	      String lowercase = "[a-z]";
 	      String digit = "\\d";
 	      String specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+	      
 	}
 	public static void main(String[] args) {
 		
