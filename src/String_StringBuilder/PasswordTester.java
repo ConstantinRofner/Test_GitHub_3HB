f056fbd0a53570bb22b14254ac6dad33f9542872
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-04-23 09:34:19 +0200

    added criterias for the password

--- src/String_StringBuilder/PasswordTester.java
+++ src/String_StringBuilder/PasswordTester.java
@@ -2,7 +2,11 @@ package String_StringBuilder;
 
 public class PasswordTester {
 	public static String isGoodPassword(String password) {
-
+		  String length = ".{8,}";
+	      String uppercase = "[A-Z]";
+	      String lowercase = "[a-z]";
+	      String digit = "\\d";
+	      String specialChar = "[!@#$%^&*]";
 	}
 	public static void main(String[] args) {
 		
