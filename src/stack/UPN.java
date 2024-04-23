f16c39ff8f63cd4e87b1b08bae109d69baf6bbcd
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-04-23 08:58:47 +0200

    added cases

--- src/stack/UPN.java
+++ src/stack/UPN.java
@@ -1,6 +1,7 @@
 package stack;
 
 import java.util.LinkedList;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UPN {
@@ -12,8 +13,23 @@ public class UPN {
 		//numericPattern: //d...Ziffer[0-9], + beliebig viele Ziffern
 		Pattern numericPattern = Pattern.compile("\\d+");
 		
+		// \\s+...teilt den String auch nach mehreren Leerzeichen
 		for(String token : input.split("\\s+")) {
-			
+			Matcher operatorMatcher = operatorPattern.matcher(token);
+			Matcher numericMatcher = numericPattern.matcher(token);
+			if(numericMatcher.matches()) {
+				stack.push(Integer.parseInt(token));
+			}else if(operatorMatcher.matches()){
+				int operand1 = stack.pop();
+				int operand2 = stack.pop();
+				int result;
+				switch(token) {
+				case "+": result = operand1 + operand2; break;
+				case "-": result = operand1 - operand2; break;
+				case "*": result = operand1 * operand2; break;
+				case "/": result = operand1 / operand2; break
+				}
+			}
 		}
 	}
 	
