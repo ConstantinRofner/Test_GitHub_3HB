2a166b1c810b2ec865b9d3485250f555e250b38e
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-04-28 15:11:07 +0200

    added a LinkedList

--- src/String_StringBuilder/SumOfTheDigits.java
+++ src/String_StringBuilder/SumOfTheDigits.java
@@ -1,8 +1,18 @@
 package String_StringBuilder;
 
+import java.util.LinkedList;
+
 public class SumOfTheDigits {
 
 	public static void main(String[] args) {
-		
+	
+		public static int digitSum(long value) {
+			String str = Long.toString(value);
+			LinkedList<Integer> stack = new LinkedList<>();
+			
+			for (String s : str.split("")) {
+				stack.push(Integer.parseInt(s));
+			}
 	}
+	
 }
