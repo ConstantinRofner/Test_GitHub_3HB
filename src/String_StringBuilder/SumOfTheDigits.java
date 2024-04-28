23302a1b7dce9ea6bab5c94a8c85518094dbeca8
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-04-28 15:16:01 +0200

    added a method to calculate the stack size

--- src/String_StringBuilder/SumOfTheDigits.java
+++ src/String_StringBuilder/SumOfTheDigits.java
@@ -20,10 +20,22 @@ public class SumOfTheDigits {
 		return stack.pop();
 	}
 	
-	public static void main(String[] args) {
+	public static int digitSum(String value) {
 		LinkedList<Integer> stack = new LinkedList<>();
 		for (String s : value.split("")) {
 			stack.push(Integer.parseInt(s));
 		}
+		while (!(stack.size() == 1)) {
+			int num1 = stack.pop();
+			int num2 = stack.pop();
+			int result = num1 + num2;
+			stack.push(result);
+		}
+		return stack.pop();
+	}
+	
+	public static void main(String[] args) {
+
+	}
 	}
 }
