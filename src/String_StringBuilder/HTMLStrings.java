eb065752aa46bc3a9d4307e9c86ae5e4103216ab
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-04-29 10:13:59 +0200

    added a if

--- src/String_StringBuilder/HTMLStrings.java
+++ src/String_StringBuilder/HTMLStrings.java
@@ -1,7 +1,12 @@
 package String_StringBuilder;
 
 public class HTMLStrings {
-
+	
+	public String htmlElement(String tag, String body){
+		if(tag == null || tag.isBlank()) {
+			return body;
+		}
+	}
 	public static void main(String[] args) {
 		
 	}
