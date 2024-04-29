ae559cbdcb8a34973f464d072b86434aafa4b3d0
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-04-29 10:28:12 +0200

    added another if and else

--- src/String_StringBuilder/HTMLStrings.java
+++ src/String_StringBuilder/HTMLStrings.java
@@ -3,12 +3,14 @@ package String_StringBuilder;
 public class HTMLStrings {
 	
 	public String htmlElement(String tag, String body){
-		if(tag == null || tag.isBlank()) {
+		if(body == null && (tag.isBlank() || tag == null)) {
+			return "";
+		}else if(tag == null || tag.isBlank()) {
 			return body;
-		}else if(body.isBlank()){
+		}else if(body == null){
 			return "<" + tag + ">" + "<" + "/" + tag + ">";
-		}else if(body.isBlank() && (tag.isBlank() || tag == null)) {
-			
+		}else {
+			return "<" + tag + ">" + body + "<" + "/" + tag + ">";
 		}
 	}
 	public static void main(String[] args) {
