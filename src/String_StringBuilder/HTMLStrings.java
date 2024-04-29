e8411b34a609753f35980f40daa2fbaa41f61e44
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-04-29 10:20:38 +0200

    added a if loop

--- src/String_StringBuilder/HTMLStrings.java
+++ src/String_StringBuilder/HTMLStrings.java
@@ -5,6 +5,10 @@ public class HTMLStrings {
 	public String htmlElement(String tag, String body){
 		if(tag == null || tag.isBlank()) {
 			return body;
+		}else if(body.isBlank()){
+			return "<" + tag + ">" + "<" + "/" + tag + ">";
+		}else if(body.isBlank() && (tag.isBlank() || tag == null)) {
+			
 		}
 	}
 	public static void main(String[] args) {
