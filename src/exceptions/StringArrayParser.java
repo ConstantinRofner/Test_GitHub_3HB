c8c07482faeb8968394b6296f18274e4aa09b00f
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-05-14 13:21:49 +0200

    added catch/try

--- src/exceptions/StringArrayParser.java
+++ src/exceptions/StringArrayParser.java
@@ -3,6 +3,14 @@ package exceptions;
 import java.util.Arrays;
 
 public class StringArrayParser {
+	
+	private static int parseStringToInt(String number, int defaultValue) {
+		try {
+			return Integer.parseInt(number);
+		}catch(NumberFormatException e) {
+			return defaultValue;
+		}
+	}
 
 	public static int[] parseInts(String...numbers) {
 		int[] intNumbers = new int[numbers.length];
@@ -15,5 +23,7 @@ public class StringArrayParser {
 	public static void main(String[] args) {
 		int[] test1 = parseInts("12", "123", "345");
 		System.out.println(Arrays.toString(test1));
+		int[] test2 = parseInts("1", "aa45", "34");
+		System.out.println(Arrays.toString(test2));
 	}
 }
