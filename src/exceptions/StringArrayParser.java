df3405755c661ec60944e6824dc90a648cbc7060
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-05-14 13:25:28 +0200

    added

--- src/exceptions/StringArrayParser.java
+++ src/exceptions/StringArrayParser.java
@@ -15,7 +15,7 @@ public class StringArrayParser {
 	public static int[] parseInts(String...numbers) {
 		int[] intNumbers = new int[numbers.length];
 		for(int i=0; i<numbers.length; i++) {
-			intNumbers[i] = Integer.parseInt(numbers[i]);
+			intNumbers[i] = parseStringToInt(numbers[i], 0);
 		}
 		return intNumbers;
 	}
