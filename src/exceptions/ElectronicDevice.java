45730876748a13fb197f2d7fdc6403811f569832
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-05-14 13:27:03 +0200

    added new class

---
+++ src/exceptions/ElectronicDevice.java
@@ -0,0 +1,22 @@
+package exceptions;
+
+public class ElectronicDevice {
+
+	private String name;
+	private int watt;
+	
+	public ElectronicDevice() {}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getWatt() {
+		return watt;
+	}
+	public void setWatt(int watt) {
+		this.watt = watt;
+	}
+}
