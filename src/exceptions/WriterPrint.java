c45570619c3a7cc569f4af5ed41fc4ad70aae6ab
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-05-21 13:21:57 +0200

    new class

--- src/exceptions/WriterPrint.java
+++ src/exceptions/WriterPrint.java
@@ -1,5 +1,16 @@
 package exceptions;
 
-public class WriterPrint {
+import java.io.PrintWriter;
+import java.io.File;
+import java.time.LocalDateTime;
 
+public class WriterPrint {
+	
+    public static void main(String[] args) {
+        try (PrintWriter writer = new PrintWriter(new File("datum.txt"), "UTF-8")) {
+            writer.println(LocalDateTime.now());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
