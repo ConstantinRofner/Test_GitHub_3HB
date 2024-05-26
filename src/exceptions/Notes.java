9084c857cda4ba1b33890b6fb66faac14c56a998
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-05-26 13:50:01 +0200

    added an scanner

--- src/exceptions/Notes.java
+++ src/exceptions/Notes.java
@@ -1,12 +1,11 @@
 package exceptions;
 
-import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Notes {
 
     public static void main(String[] args) {
-    	Scanner sc = new Scanner(Paths.get("file.txt"));
+    	Scanner sc;
     	
     }
 }
