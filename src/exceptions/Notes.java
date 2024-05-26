334d1aad1aa40fdc5c68a0d5d26300811d3d6e38
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-05-26 13:48:50 +0200

    created class notes

--- src/exceptions/Notes.java
+++ src/exceptions/Notes.java
@@ -1,11 +1,12 @@
 package exceptions;
 
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Notes {
 
     public static void main(String[] args) {
-    	Scanner sc = new Scanner();
+    	Scanner sc = new Scanner(Paths.get("file.txt"));
     	
     }
 }
