2a76476163dcdac065af2c0284d3215aca14c960
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-05-26 13:52:45 +0200

    added a try method

--- src/exceptions/Notes.java
+++ src/exceptions/Notes.java
@@ -1,11 +1,17 @@
 package exceptions;
 
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Notes {
 
     public static void main(String[] args) {
     	Scanner sc;
-    	
+    	try {
+    		sc = new Scanner(Paths.get("file.txt"));
+    		while(sc.hasNextLine());{
+    			System.out.println(sc.nextLine() + " ");
+    		}
+    	}
     }
 }
