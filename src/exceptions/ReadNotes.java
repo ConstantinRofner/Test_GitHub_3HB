4e2db7d561ce8cac8ac05f55337a335ae1ad2164
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-05-26 13:55:24 +0200

    renamed class

--- src/exceptions/ReadNotes.java
+++ src/exceptions/ReadNotes.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
-public class Notes {
+public class ReadNotes {
 
     public static void main(String[] args) {
     	Scanner sc;
